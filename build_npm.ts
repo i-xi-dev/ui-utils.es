62cba9a39559025391c137bee2cc543600b4079c
i-xi-dev <naoyuki.isono@gmail.com>
2023-11-12 01:30:12 +0900

    2.3.6

--- build_npm.ts
+++ build_npm.ts
@@ -15,7 +15,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/ui-utils",
-    version: "2.3.5",
+    version: "2.3.6",
     description: "This is not for direct usage.",
     license: "MIT",
     author: "i-xi-dev",
